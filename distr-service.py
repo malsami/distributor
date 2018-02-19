a131de0c4db990e36b3f59bbaa11669e269b079c
RobertHa <robert.hamsch@tum.de>
2018-02-19 11:22:24 +0100

    added function skeleton and defined functionality

--- distr-service.py
+++ distr-service.py
@@ -2,22 +2,23 @@ from service import Service, find_syslog
 import logging
 from logging.handlers import SysLogHandler
 import time
+from distributor import Distributor
+
 class DistributorService(Service):
 	def __init__(self, *args, **kwargs):
 		super(DistributorService, self).__init__(*args,**kwargs)
 		#TODO understand logger and use correct loghandlers
 		self.logger.addHandler(SysLogHandler(address=find_syslog(), facility=SysLogHandler.LOG_DAEMON))
 		self.logger.setLevel(logging.INFO)
+		self.distributor = Distributor()#TODO use this? https://stackoverflow.com/questions/865115/how-do-i-correctly-clean-up-a-python-object
 
 
 	def run(self):
 		n = 0
 		while not self.got_sigterm():
 			self.logger.info("I'm working...")
-			"""
-			TODO
-			here we want to create the distributor instance
-			"""
+			#TODO or should the distributor be created in run()
+		self.logger.info("I stopped")
 
 if __name__ == '__main__':
 	import sys
@@ -28,24 +29,45 @@ if __name__ == '__main__':
 	cmd = sys.argv[1].lower()
 	service = DistributorService('distr_service', pid_dir='/tmp')
 
-	if cmd == 'start':
+	if cmd == 'service_start':
 		service.start()
 		with open('/home/hamsch/operating-system/client-tools/service/try1.log', 'w') as f:#only for testing something
 				f.write("still here {}\n".format(0))
 		print("i got told to start")
 
-	elif cmd == 'stop':
+	elif cmd == 'service_stop':
+		#TODO: close distributor first
 		service.stop()
 
+	elif cmd == 'service_status':
+		if service.is_running():
+			print("Service is running.")
+		else:
+			print("Service is not running.")
+
+	elif cmd == 'add_job':
+		#TODO check for number of arguments ( taskset, monitor, *session_params)
+		#TODO call add_job(taskset, monitor, *session_params) on the distributor
+
+	elif cmd == 'check_state':
+		#TODO call get_distributor_state() on distributor
+		#	print message accordingly
+
+	elif cmd == 'get_max_machine_value':
+		#TODO call get_max_machine_value() on distributor
+
+	elif cmd == 'change_max_machine_value':
+		#TODO call set_max_machine_value(new_value) on distributor
+
+
+	elif cmd == 'help':
+		#TODO print commands and what they are for
+
+	#TODO discard this after debugging
 	elif cmd == 'custom':
 		log = service.logger.handlers
 		for h in log:
 			print(h.format())
 
-	elif cmd == 'status':
-		if service.is_running():
-			print("Service is running.")
-		else:
-			print("Service is not running.")
 	else:
 		sys.exit('Unknown command "%s".' % cmd)
