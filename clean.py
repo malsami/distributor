19ce2751e9aa76ef4c3f66b3c2967318ae3815c3
RobertHa <robert.hamsch@tum.de>
2018-07-04 18:21:03 +0200

    and also changed to kill via pid in clean

--- clean.py
+++ clean.py
@@ -5,6 +5,7 @@ for i in range(1, 1 + int(sys.argv[1])):
 	screens = Popen(['./grep_screen.sh', str(i)], stdout=PIPE, stderr=PIPE).communicate()[0].split()
 	print(i, screens)
 	for screen in screens:
-		Popen(['screen', '-X','-S', str(screen,'utf-8'), 'kill'])
+		Popen(['kill', '-9', str(screen, 'utf-8')])
+		#Popen(['screen', '-X','-S', str(screen,'utf-8'), 'kill'])
 	Popen(['sudo', 'ip', 'link', 'delete', 'tap{}'.format(i)])
 
