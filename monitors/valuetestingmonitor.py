dbe3e56bce0ead3ae0396dd35fcc80a7643d3b19
RobertHa <robert.hamsch@tum.de>
2018-08-31 16:40:43 +0200

    added monitor

---
+++ monitors/valuetestingmonitor.py
@@ -0,0 +1,46 @@
+
+import sys
+sys.path.append('../')
+from distributor_service.monitor import AbstractMonitor
+import logging
+
+class ValueTestingMonitor(AbstractMonitor):
+    
+    def __init__(self, output_list):
+        self.out = output_list
+
+        
+    def __taskset_event__(self, taskset):
+        pass
+
+
+    def __taskset_start__(self, taskset):
+        pass
+
+
+    def __taskset_finish__(self, taskset):
+        ret_dict = {}
+        for task in taskset:
+            j = []
+            for job in task.jobs:
+                j.append((job.start_date, job.end_date, job.exit_value))
+            ret_dict[str(task.id)] = (task,j)
+        self.out.append((0,ret_dict))
+
+    def __taskset_stop__(self, taskset):
+        for task in taskset:
+            task.jobs = []
+        pass
+
+    def __taskset_bad__(self, taskset, n):
+        """After n tries the taskset could not be executedcorrectly"""
+        ret_dict = {}
+        for task in taskset:
+            j = []
+            for job in task.jobs:
+                j.append((job.start_date, job.end_date, job.exit_value))
+            ret_dict[str(task.id)] = (task,j)
+        self.out.append((n,ret_dict))
+
+
+
